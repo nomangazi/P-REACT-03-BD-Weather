3d2438a41669b459cefc84f4f7c397dcc1195d34
Noman Gazi <numan.islam61@gmail.com>
2024-02-09 21:16:35 +0600

    update

--- src/context/InitProvider.jsx
+++ src/context/InitProvider.jsx
@@ -1,7 +1,20 @@
-function InitProvider() {
-  return (
-    <div>InitProvider</div>
-  )
+import { createContext, useEffect, useMemo, useState } from 'react';
+
+const InitContext = createContext();
+export const useInit = () => useContext(InitContext);
+
+function InitProvider({ children }) {
+    const [initData, setInitData] = useState(null);
+    const [locationInfo, setLocationInfo] = useState(null);
+    useEffect(() => {
+        console.log(navigator);
+    }, []);
+
+    const value = useMemo(() => {
+        return { initData, setInitData, locationInfo, setLocationInfo };
+    }, []);
+
+    return <InitContext.Provider value={value}>{children}</InitContext.Provider>;
 }
 
-export default InitProvider
+export default InitProvider;
--- src/projects/App.jsx
+++ src/projects/App.jsx
@@ -1,19 +1,20 @@
-
+import InitProvider from '../context/InitProvider';
 import ComposeProviders from './utils/ComposeProviders';
 
 function App() {
     // // -- Change fav icon dynamically
     // var link = document.querySelector("link[rel~='icon'");
     // link.href = "https://files.bikiran.com/assets/images/icon/icon-bik-logoV1.svg";
-  return (
-    <ComposeProviders components={[/** All Global Provider here */]}>
-        
-        <h1 className='w-full text-7xl font-bold text-center h-[86vh] inline-flex flex-col justify-center items-center gap-2'>
-            <span>Hello Developers 👋</span>
-            <div className='font-medium text-base text-gray-600 text-start'>This template are using vite, tailwind, react</div>
-        </h1>
-    </ComposeProviders>
-  )
+    return (
+        <ComposeProviders components={[InitProvider]}>
+            <h1 className="w-full text-7xl font-bold text-center h-[86vh] inline-flex flex-col justify-center items-center gap-2">
+                <span>Hello Developers 👋</span>
+                <div className="font-medium text-base text-gray-600 text-start">
+                    This template are using vite, tailwind, react
+                </div>
+            </h1>
+        </ComposeProviders>
+    );
 }
 
-export default App
+export default App;
