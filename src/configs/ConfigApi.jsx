42f1b6fbce694c187f49c77cadd0bdb422a65162
Noman Gazi <numan.islam61@gmail.com>
2024-02-09 20:08:59 +0600

    remove old file

--- src/configs/ConfigApi.jsx
+++
@@ -1,9 +0,0 @@
-export const api = process.env.REACT_APP_BASE_URL;
-export const key = process.env.REACT_APP_API_KEY;
-
-const ConfigApi = {
-    // Search
-    SEARCH: '/search/'
-};
-
-export default ConfigApi;
